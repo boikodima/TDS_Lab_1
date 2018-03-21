c50461355f1cb32d98e24d6c823c213232a2be76
KathrynYakovenko <kathryn.yakovenko@gmail.com>
2018-03-21 20:51:49 +0200

    KateCommit=)

--- Lab-1.cs
+++ Lab-1.cs
@@ -6,7 +6,26 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-			
+            int[] myArr = new int[50];
+            Random rand = new Random();
+
+
+            #region Bubblesort
+            Console.WriteLine("Array before sort");
+
+            for (int i = 0; i < 50; i++)
+            {
+                myArr[i] = rand.Next(100);
+                Console.Write(myArr[i] + " ");
+            }
+
+            BubbleSort(myArr);
+            Console.WriteLine("\n\nArray after bubblesort");
+            for (int i = 0; i < 50; i++)
+            {
+            Console.Write(myArr[i] + " ");
+            }
+            #endregion
 		}
 	}
 }
